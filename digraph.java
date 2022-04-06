eb7385e7556cc9189544c0e9bf0b6282799bd524
Pranav <86232869+BollinaPranav@users.noreply.github.com>
2022-04-06 23:17:55 +0100

    Add files via upload

--- digraph.java
+++ digraph.java
@@ -1,42 +1,73 @@
 import java.util.*;
 import java.io.*;
 public class digraph {
-	 private Map<String, Integer> stops;
-	    //This Map will holds key value pairs of a stops ID and its associated index in the adjacency list.
-	    private Map<Integer, Integer> stopIndexes;
-	    //This Map will hold key value pairs of an index in the array and its associated stop name (necessary for getting the correct stops from the graph).
-	    private Map<Integer, String> indexToName;
+	
 	    //The Array-list below will be an adjacency list for representing the graph.
-	    private ArrayList<edge>[] adj;
-	    private int V;
+	    public Map <Integer,List<edge>> adjs;
+	    int vertices = 12480;
 	    
-
-    public digraph(String stopsFile, String stopTimesFile, String transfersFile) throws IOException {
-    	 stops = new HashMap<String, Integer>();
-         stopIndexes = new HashMap<Integer, Integer>();
-         indexToName = new HashMap<Integer, String>();
-         
-         BufferedReader reader = new BufferedReader(new FileReader(stopsFile));
-         reader.readLine();
-         String line = null;
-         String[] splitLine = null;
-         int vertex = 0;
-         int ID = 0;
-         String name = null;
-         while((line = reader.readLine()) != null)
-         {
-             splitLine = line.split(",");                       //split the line based on commas
-             name = splitLine[2];                               // get the name of the stop  
-             ID = Integer.parseInt(splitLine[0]);               // get the stop id
-             stops.put(name, ID);                               // put stop id and name  
-             stopIndexes.put(ID, vertex);                       // put the ID and vertex(count)
-             indexToName.put(vertex, name);                     // relate the vertex to the name 
-             vertex++;
-         }
-        		reader.close();
-        		V = vertex;                                     // total number of vertices
-        		
-        		
-    }
+	public digraph(String stopsFile, String stopTimesFile, String transfersFile) throws IOException {
+		// TODO Auto-generated constructor stub
+		
+   	 
+    		
+     BufferedReader reader = new BufferedReader(new FileReader(stopTimesFile));
+    		reader.readLine();
+    		String line = null;
+    		String[] splitLine = null;
+    		adjs = new HashMap<>();
+            int lastTripID = -1;
+            int currentTripID = 0;
+            int toID = 0;
+            int fromID = 0;
+            int fromIndex = 0;
+            int toIndex = 0;
+            while((line = reader.readLine())!=null)
+            {
+            	splitLine = line.split(",");
+            	currentTripID = Integer.parseInt(splitLine[0]);
+            	toID = Integer.parseInt(splitLine[3]);
+            	if(currentTripID == lastTripID)
+                {
+                   
+            		List<edge> adjList = adjs.getOrDefault(fromID, new ArrayList<>());
+                    adjList.add(new edge(fromID,toID,1.0));
+                    adjs.put(fromID, adjList);
+                }
+                lastTripID = currentTripID;
+                fromID = toID;
+            }
+            reader.close();
+            
+            reader = new BufferedReader(new FileReader(transfersFile));
+             toID = 0;
+             fromID = 0;
+             double weight =0;
+             reader.readLine();
+            line = null;
+     		splitLine = null;
+     		while((line = reader.readLine())!=null)
+            {
+     			
+     			splitLine = line.split(",");
+     			fromID = Integer.parseInt(splitLine[0]);
+     			toID = Integer.parseInt(splitLine[1]);
+     			if(Integer.parseInt(splitLine[2])==0)
+     			{
+     				weight = 2;
+     			}
+     			else
+     			{
+     				weight = Double.parseDouble(splitLine[3])/100;
+     			}
+     			List<edge> adjList = adjs.getOrDefault(fromID, new ArrayList<>());
+                adjList.add(new edge(fromID,toID,weight));
+                adjs.put(fromID, adjList);
+            }
+     		reader.close();
+            
+            System.out.println("hello");
+	}
+    
 
 }
