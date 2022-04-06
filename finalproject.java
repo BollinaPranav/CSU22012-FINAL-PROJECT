b2056ddf04bf5846e4bd4716f6601f3b03dc8750
Pranav <86232869+BollinaPranav@users.noreply.github.com>
2022-04-06 23:56:53 +0100

    Add files via upload

--- finalproject.java
+++ finalproject.java
@@ -2,25 +2,43 @@ import java.util.*;
 import java.io.*;
 public class finalproject {
 
-	static digraph digraph;
+	 static digraph digraph;
 	public static void main(String[]Args)
 	{
+		
 		try {
-			digraph = new digraph("stop_times.txt");
+			digraph = new digraph ("stops.txt","stop_times.txt","transfers.txt");
+			System.out.print("Input from bus stopID: ");
+			Scanner input = new Scanner(System.in);
+			int start_id = input.nextInt();
+			System.out.print("\nInput to bus stopID: ");
+			double[] distances = DijkstraShortestPath(start_id);
+			int stop_id = input.nextInt();
+			System.out.println(distances[stop_id]);
+			getPath(stop_id,start_id);
+			
+			
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	
+	private static void getPath(int stopID, int startID) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<Integer> path = new ArrayList<Integer>();
+		path.add(startID);
+		
+		
+	}
 
-
-	private double[] DijkstraShortestPath(int sourceNode)
+	private static double[] DijkstraShortestPath(int sourceNode)
 	{
-		Map<Integer, List<edge>> adjList = graph.adjs;
-		boolean[] observed = new boolean [graph.vertices];
-		double[] toDist = new double[graph.vertices];
+		Map<Integer, List<edge>> adjList = digraph.adjs;
+		boolean[] observed = new boolean [digraph.vertices];
+		double[] toDist = new double[digraph.vertices];
 		double infinity = Double.POSITIVE_INFINITY;
 		Arrays.fill(toDist, infinity);                          //initialize all the distances to infinfity
 		toDist[sourceNode] = 0;                                 // source node's distance to itself is 0  
@@ -50,5 +68,7 @@ public class finalproject {
 
 		return toDist;
 	}
+	
+	
 }
 
