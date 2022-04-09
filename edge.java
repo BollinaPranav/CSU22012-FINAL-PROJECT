73a859dab689a5db89fff7496413c43a1bcc7deb
Pranav <86232869+BollinaPranav@users.noreply.github.com>
2022-04-09 09:19:37 +0100

    Add files via upload

---
+++ edge.java
@@ -0,0 +1,15 @@
+
+public class edge {
+	public int FromEdge;
+    public int ToEdge;
+    public double cost;
+
+	public edge(int FromEdge, int ToEdge, double cost) {
+		this.FromEdge = FromEdge;
+		this.ToEdge = ToEdge;
+		this.cost = cost;
+	}
+	
+	
+
+}
